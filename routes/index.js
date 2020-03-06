883a76c407adb58be7d8599c63fe53a73b0af8e7
Rob Thuleen <robert@diversyfund.com>
2020-03-06 11:33:40 -0800

    Added env message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', env: process.env.MY_ENV_VAR || 'dev' });
 });
 
 module.exports = router;
