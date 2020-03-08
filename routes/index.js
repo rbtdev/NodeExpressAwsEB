3b0fd4ce5383c501a071e64e0be86af671707d6c
Rob Thuleen <rob.thuleen@gmail.com>
2020-03-08 12:43:54 -0700

    update env output

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let { NODE_ENV, 
-    RDS_DB_NAME,
-    RDS_USERNAME,
-    RDS_PASSWORD,
+  let { NODE_ENV = 'development', 
+    // RDS_DB_NAME,
+    // RDS_USERNAME,
+    // RDS_PASSWORD,
     RDS_HOSTNAME,
-    RDS_PORT
+    // RDS_PORT
   } = process.env;
 
   let env = {
     NODE_ENV, 
-    RDS_DB_NAME,
-    RDS_USERNAME,
-    RDS_PASSWORD,
+    // RDS_DB_NAME,
+    // RDS_USERNAME,
+    // RDS_PASSWORD,
     RDS_HOSTNAME,
-    RDS_PORT
+    // RDS_PORT
   }
 
   console.log(env)
